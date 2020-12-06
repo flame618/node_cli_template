fb1029bd90057c30adb3e2111a980d02c5af92bf
flame618 <859057040@qq.com>
2020-12-06 20:15:31 +0800

    feat: update

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,16 @@ const program = require("commander");
 function initProgram() {
   program.version("1.0.0");
   /**写入命令代码 */
+  program
+    .command("init <name>")
+    .option("-r, --param1 <param1>", "参数1")
+    .option("-o, --param2 <param2>", "参数2")
+    .action(async (name, obj) => {
+      console.log(name);
+      console.log(obj.param1);
+      console.log(obj.param2);
+    });
+  
   program.parse(process.argv);
 }
 
