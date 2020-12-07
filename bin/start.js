b4521d39ed45963e2f7af3cc6bdf1a92e53e62c2
flame618 <859057040@qq.com>
2020-12-07 22:08:29 +0800

    feat: update

--- bin/start.js
+++ bin/start.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-const { initProgram } = require("../app/scripts/index");
+const { initProgram } = require("../src/index");
 
 initProgram();
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const program = require("commander");
+const pkg = require("../package.json")
 function initProgram() {
-  program.version("1.0.0");
+  program.version(pkg.version);
   /**写入命令代码 */
   program
     .command("init <name>")
     .option("-r, --param1 <param1>", "参数1")
     .option("-o, --param2 <param2>", "参数2")
+    .option("-i --param3", "参数3")
     .action(async (name, obj) => {
       console.log(name);
       console.log(obj.param1);
       console.log(obj.param2);
+      console.log(obj.param3);
     });
   
   program.parse(process.argv);
